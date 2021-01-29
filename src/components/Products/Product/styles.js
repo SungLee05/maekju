4caf6b8322f531a58ed1221e9e62138d9eda52e9
Sung Lee <sungyonglee414@gmail.com>
2021-01-29 10:52:20 -0800

    feat: single product cards added

---
+++ src/components/Products/Product/styles.js
@@ -0,0 +1,21 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  root: {
+    maxWidth: "100%",
+  },
+  media: {
+    height: 10,
+    maxWidth: 100,
+    paddingTop: "30px",
+    paddingBottom: "100%",
+  },
+  cardActions: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  cardContent: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+}));
