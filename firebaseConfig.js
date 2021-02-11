6d7323c2198d7f9d3051c6b2aaa045437b1570b8
Sung Lee <sungyonglee414@gmail.com>
2021-02-10 16:43:29 -0800

    feat: FB added.

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDmTFdcIMVO8DhO8I3t2KQI4k9L8t0P_k0",
+  authDomain: "maekju-cef47.firebaseapp.com",
+  projectId: "maekju-cef47",
+  storageBucket: "maekju-cef47.appspot.com",
+  messagingSenderId: "998152128219",
+  appId: "1:998152128219:web:82efea0ff31ae9c5252efe",
+};
